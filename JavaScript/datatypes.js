51b5d7fa4278b81cf25d34aeedb165646f5e875e
kavin-2304 <kaavin2304@gmail.com>
2026-01-20 16:50:02 +0530

    DataTypes Files added

---
+++ JavaScript/datatypes.js
@@ -0,0 +1,20 @@
+let studentName='Kavin';
+let Rollno=101;
+let result=true;
+
+console.log(`My name is ${studentName} and my roll number is :${Rollno}`)
+
+if(result==true){
+    console.log('Pass');
+}else{
+    console.log('Fail');
+}
+
+let student = {
+    name:'Yagnesh',
+    Rollno:202,
+    marks:85,
+    subjects:['Maths','Physcis','Chemistry'],
+}
+
+console.log(student.subjects);

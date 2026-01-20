28fdcd069328b5a487c427e85c973f346e68eed2
kavin-2304 <kaavin2304@gmail.com>
2026-01-20 16:32:13 +0530

    JavaScript folder Added

---
+++ JavaScript/syntaxAndVariables.js
@@ -0,0 +1,13 @@
+var name='yagnesh';
+var name = 'kavin';
+console.log(`My name is Kavin :${name}`);
+// in var redeclartion is allowed but using var is not recommended 
+
+let name2 = 'kavin';
+name2='yagnesh';
+console.log(name2);
+// let variables can be modified but not redecalred. it will throw an error
+
+const name3='wohlig';
+console.log(name3);
+// variables cannot be modified nor redeclared

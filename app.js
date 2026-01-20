364dd22fa410dbac0fd63731737714d6bb245946
kavin-2304 <kaavin2304@gmail.com>
2026-01-20 14:30:50 +0530

    Api fetch method added

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-//adding a new Feature 
-console.log("hello World");
+const URL='http://api.football-data.org/v4/competitions/'
+
+async function getData(){
+    try{
+        let data=await fetch(URL);
+        console.log(data);
+    }catch(err){
+        console.log(err);
+    }
+}
